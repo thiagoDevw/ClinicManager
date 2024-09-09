db775a819658dd566e6b529d8823a4159f213a3f
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-09-09 14:33:45 -0300

    Novo commit

--- ClinicManager.Application/Services/NotificationBackgroundService.cs
+++ ClinicManager.Application/Services/NotificationBackgroundService.cs
@@ -24,10 +24,14 @@ namespace ClinicManager.Application.Services
 
         private void BackgroundProcessing(CancellationToken stoppingToken)
         {
+            Console.WriteLine("Serviço de background iniciado...");
+
             while (!stoppingToken.IsCancellationRequested)
             {
+                Console.WriteLine("Executando o serviço de Background...");
+
                 // Rodar a cada 24 horas
-                Thread.Sleep(TimeSpan.FromHours(1));
+                Thread.Sleep(TimeSpan.FromMinutes(5));
 
                 // Executar a lógica de notificação
                 NotifyPatients();
@@ -36,21 +40,28 @@ namespace ClinicManager.Application.Services
 
         private void NotifyPatients()
         {
+            Console.WriteLine("Notificando paciente");
+
             using (var scope = _serviceProvider.CreateScope())
             {
-                var dbContext = scope.ServiceProvider.GetRequiredService<ClinicDbContext>;
+                var dbContext = scope.ServiceProvider.GetRequiredService<ClinicDbContext>();
                 var emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
 
                 // Buscar os atendimentos do dia seguinte
-                var targetTime = DateTime.Now.AddMinutes(2);
+                var now = DateTime.Now;
+                var targetTime = now.AddMinutes(5);
+
                 var appointments = dbContext.CustomerServices
                     .Include(c => c.Patient)
                     .Where(c => c.Start >= DateTime.Now && c.Start <= targetTime)
                     .ToList();
-                
+
+                Console.WriteLine($"Número de atendimentos encontrados: {appointments.Count}");
+
                 foreach (var appointment in appointments)
                 {
                     var patient = appointment.Patient;
+                    Console.WriteLine($"Notificando paciente: {patient.Name}");
                     var subject = "Lembrete da consulta";
                     var message = $"Olá {patient.Name}, este é um lembrete de sua consulta marcada para {appointment.Start}.";
 
