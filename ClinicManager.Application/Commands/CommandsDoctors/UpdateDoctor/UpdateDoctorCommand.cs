b8ee74ade18f696c439507c6f8e57f747a8b2662
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-09-13 17:19:49 -0300

    Novo commit

---
+++ ClinicManager.Application/Commands/CommandsDoctors/UpdateDoctor/UpdateDoctorCommand.cs
@@ -0,0 +1,43 @@
+﻿using ClinicManager.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.Application.Commands.CommandsDoctors.UpdateDoctor
+{
+    public class UpdateDoctorCommand : IRequest<ResultViewModel<int>>
+    {
+        public UpdateDoctorCommand(int id)
+        {
+            Id = id;
+        }
+
+        public UpdateDoctorCommand(string name, string lastName, DateTime dateOfBirth, string phone, string email, string cPF, string bloodType, string address, string specialty, string cRM)
+        {
+            Name = name;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Phone = phone;
+            Email = email;
+            CPF = cPF;
+            BloodType = bloodType;
+            Address = address;
+            Specialty = specialty;
+            CRM = cRM;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public string BloodType { get; set; }
+        public string Address { get; set; }
+        public string Specialty { get; set; }
+        public string CRM { get; set; }
+    }
+}
---
+++ ClinicManager.Application/Commands/CommandsDoctors/UpdateDoctor/UpdateDoctorHandler.cs
@@ -0,0 +1,49 @@
+﻿using ClinicManager.Application.Models;
+using ClinicManager.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManager.Application.Commands.CommandsDoctors.UpdateDoctor
+{
+    public class UpdateDoctorHandler : IRequestHandler<UpdateDoctorCommand, ResultViewModel<int>>
+    {
+        private readonly ClinicDbContext _context;
+
+        public UpdateDoctorHandler(ClinicDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(UpdateDoctorCommand request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                return ResultViewModel<int>.Error("Os dados do médico são obrigatórios.");
+            }
+
+            var doctor = await _context.Doctors.SingleOrDefaultAsync(d => d.Id == request.Id);
+
+            if (doctor == null)
+            {
+                return ResultViewModel<int>.Error("Médico não encontrado.");
+
+            }
+
+            doctor.Name = request.Name;
+            doctor.LastName = request.LastName;
+            doctor.DateOfBirth = request.DateOfBirth;
+            doctor.Phone = request.Phone;
+            doctor.Email = request.Email;
+            doctor.CPF = request.CPF;
+            doctor.BloodType = request.BloodType;
+            doctor.Address = request.Address;
+            doctor.Specialty = request.Specialty;
+            doctor.CRM = request.CRM;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+
+            return ResultViewModel<int>.Success(doctor.Id);
+        }
+    }
+}
