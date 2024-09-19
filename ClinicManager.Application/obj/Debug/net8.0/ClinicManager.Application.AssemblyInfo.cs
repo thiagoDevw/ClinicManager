d69a914a80097097a1250d10a98fdde8425c6537
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-09-19 18:28:03 -0300

    Novo commit

--- ClinicManager.Application/obj/Debug/net8.0/ClinicManager.Application.AssemblyInfo.cs
+++ ClinicManager.Application/obj/Debug/net8.0/ClinicManager.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicManager.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006b2470492bf382becef05209f8ce0476741fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2e067c13854e0aad6e827aa8f274a7c1efddff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicManager.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicManager.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
